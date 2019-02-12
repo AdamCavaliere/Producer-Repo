3e53648d9faa30b13fa9519ce5d2b8c86a2df028
Alex Harness <aharness@hashicorp.com>
2019-02-12 10:35:54 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,9 @@ output "main_vpc" {
   value = "${aws_vpc.main.id}"
 }
 
+output "default_security_group_id" {
+  value = "${aws_vpc.main.default_security_group_id}"
+}
 output "research_subnet_id" {
   value = "${aws_subnet.research.id}"
 }
