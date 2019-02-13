6497bf3019b30b460d70cd1e5aedd357998d635b
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2019-02-13 12:22:20 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,15 +3,15 @@ output "main_vpc" {
 }
 
 output "development_subnet_id" {
-  value = "${aws_subnet.research.id}"
+  value = "${aws_subnet.development.id}"
 }
 
 output "staging_subnet_id" {
-  value = "${aws_subnet.test.id}"
+  value = "${aws_subnet.staging.id}"
 }
 
 output "production_subnet_id" {
-  value = "${aws_subnet.prod.id}"
+  value = "${aws_subnet.production.id}"
 }
 
 output "region" {
