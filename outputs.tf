33ba5b378eac67171b1acc6371e75f7d2972e889
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-02-15 17:25:25 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "main_vpc" {
   value = "${aws_vpc.main.id}"
 }
 
+output "default_security_group_id" {
+  value = "${aws_vpc.main.default_security_group_id}"
+}
+
 output "development_subnet_id" {
   value = "${aws_subnet.development.id}"
 }
