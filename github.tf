cfd74a4be6b7b39a4f70868f87f15b1c937e183e
AdamCavaliere <adam.cavaliere@gmail.com>
2019-02-15 22:32:49 -0600

    Update github.tf

--- github.tf
+++ github.tf
@@ -1,6 +1,18 @@
+variable "git_token" {}
+variable "git_repo" {}
+
+data "template_file" "init" {
+  template = "${file("curl.tmpl")}"
+  vars = {
+    git_token = "${var.git_token}"
+    git_repo = "${var.git_repo}"
+  }
+}
+
+
 resource "null_resource" "example1" {
   provisioner "local-exec" {
-    command = "execute.sh"
+    command = "${data.template_file.init.rendered}"
   }
 }
 
