63191515172a8994861079f2856d6ee43e4a5366
AdamCavaliere <adam.cavaliere@gmail.com>
2019-02-15 23:18:00 -0600

    Update github.tf

--- github.tf
+++ github.tf
@@ -2,8 +2,8 @@ variable "git_token" {}
 variable "git_repo" {}
 variable "git_user" {}
 
-data "template_file" "init" {
-  template = "${file("curl.tmpl")}"
+data "template_file" "create" {
+  template = "${file("create_repo.tmpl")}"
   vars = {
     git_token = "${var.git_token}"
     git_repo = "${var.git_repo}"
@@ -12,9 +12,25 @@ data "template_file" "init" {
 }
 
 
-resource "null_resource" "example1" {
-  provisioner "local-exec" {
-    command = "${data.template_file.init.rendered}"
+data "template_file" "delete" {
+  template = "${file("delete_repo.tmpl")}"
+  vars = {
+    git_token = "${var.git_token}"
+    git_repo = "${var.git_repo}"
+    git_user = "${var.git_user}"
   }
+}
+
+resource "null_resource" "github_mgmt" {
+  provisioner "local-exec" {
+    command = "${data.template_file.create.rendered}"
+  }
+  
+  provisioner "local-exec"{
+    when = "destroy"
+    command = "${data.template_file.delete.rendered}"
+    
+  }
+  
 }
 
