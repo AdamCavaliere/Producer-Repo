638ce25330d29140f9b58d8bba53c00ee8ba7962
William Patton <wjpatton@wjpatton.com>
2019-09-24 07:31:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "tfe_workspace" "development" {
   organization = "${var.org}"
   auto_apply   = true
   queue_all_runs = false
-  terraform_version = "0.12.7"
+  terraform_version = "0.12.9"
 
   vcs_repo {
     branch         = "development"
@@ -77,7 +77,7 @@ resource "tfe_workspace" "staging" {
   name         = "${var.use_case_name}-staging"
   organization = "${var.org}"
   auto_apply   = true
-  terraform_version = "0.12.7"
+  terraform_version = "0.12.9"
 
   vcs_repo {
     branch         = "staging"
@@ -89,7 +89,7 @@ resource "tfe_workspace" "staging" {
 resource "tfe_workspace" "production" {
   name         = "${var.use_case_name}-production"
   organization = "${var.org}"
-  terraform_version = "0.12.7"
+  terraform_version = "0.12.9"
 
   vcs_repo {
     branch         = "master"
